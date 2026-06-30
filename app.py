a7433d231578e07e406ec5af8c00bfffa9552824
Foxyceoo <vanhidol2k3@gmail.com>
2026-07-01 04:03:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -59,8 +59,8 @@ if uploaded_file:
 
     td {{ 
         height: 50px !important; 
-        width: 27px !important;
-        min-width: 27px !important;
+        width: 25px !important;
+        min-width: 25px !important;
         vertical-align: top !important; 
         padding: 0; 
         font-weight: bold; 
