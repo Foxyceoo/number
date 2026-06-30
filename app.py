6967e1750a5105937ce7cbca963e91ca7dc6f612
Foxyceoo <vanhidol2k3@gmail.com>
2026-06-30 19:22:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import json
+import streamlit.components.v1 as components # Thêm thư viện này
 
 def get_number_from_key(key_str):
     try: return (int(key_str.split('Key')[1]) % 15) + 1
@@ -15,17 +16,16 @@ if uploaded_file := st.file_uploader("Tải file JSON", type=["json"]):
     time_map = {}
     for n in notes:
         beat_idx = round(n['time'] / beat_duration)
-        if beat_idx not in time_map: time_map[beat_idx] = []
-        time_map[beat_idx].append(get_number_from_key(n['key']))
+        time_map.setdefault(beat_idx, []).append(get_number_from_key(n['key']))
     
     max_beat = max(time_map.keys()) if time_map else 0
     
     st.subheader(f"Nhạc phổ (BPM: {bpm}) - Nhịp 4/4")
     
-    html = "<table style='border-collapse: collapse; text-align: center; font-size: 10px;'>"
+    html_content = "<table style='border-collapse: collapse; text-align: center; font-size: 10px; width: 100%;'>"
     
     for khuong in range(0, max_beat + 16, 16):
-        html += "<tr>"
+        html_content += "<tr>"
         for phach in range(khuong, khuong + 16):
             vals = sorted(time_map.get(phach, []), reverse=True, key=lambda x: int(x) if x != "" else 0)
             
@@ -33,17 +33,17 @@ if uploaded_file := st.file_uploader("Tải file JSON", type=["json"]):
             if (phach + 1) % 4 == 0: border_right = "2px solid #555"
             if (phach + 1) % 16 == 0: border_right = "4px solid black"
             
-            # SỬA LỖI Ở ĐÂY: Kiểm tra kỹ vals trước khi truy cập chỉ số
+            # Xử lý nội dung hiển thị an toàn
+            cell_content = ""
             if vals:
-                nots_str = f"{vals[0]}"
+                cell_content = f"{vals[0]}"
                 if len(vals) > 1:
-                    nots_str += "<br>" + "<br>".join(map(str, vals[1:]))
-                content = nots_str
-            else:
-                content = ""
+                    cell_content += "<br>" + "<br>".join(map(str, vals[1:]))
             
-            html += f"<td style='border-right: {border_right}; border-bottom: 1px solid #ccc; width: 30px; height: 50px; vertical-align: top;'>{content}</td>"
-        html += "</tr>"
+            html_content += f"<td style='border-right: {border_right}; border-bottom: 1px solid #ccc; width: 30px; height: 50px; vertical-align: top;'>{cell_content}</td>"
+        html_content += "</tr>"
         
-    html += "</table>"
-    st.markdown(html, unsafe_html=True)
+    html_content += "</table>"
+    
+    # Dùng components.html để nhúng trực tiếp, tránh lỗi st.markdown
+    components.html(f"<html><body>{html_content}</body></html>", height=500, scrolling=True)
