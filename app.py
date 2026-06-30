b9cf9c8355f34b353659d776997e9ab36ab89f8c
Foxyceoo <vanhidol2k3@gmail.com>
2026-06-30 18:51:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,22 @@ import json
 def draw_table(times, time_map):
     if not times: return
     
-    html = "<table style='width:100%; border-collapse: collapse; text-align: center; margin-bottom: 20px; font-size: 12px; table-layout: fixed;'>"
+    # Tạo một bảng duy nhất
+    html = "<table style='width:100%; border-collapse: collapse; text-align: center; margin-bottom: 20px; font-size: 12px;'>"
+    
+    # Chỉ tạo 2 hàng (trên và dưới)
     for row_idx in range(2):
         html += "<tr>"
+        # Các cột (t) sẽ nằm trên cùng 1 hàng này
         for t in times:
             vals = sorted(time_map.get(t, []), reverse=True, key=lambda x: int(x) if x != "" else 0)
             
-            # Xử lý nội dung an toàn hơn
             if row_idx == 0:
                 content = str(vals[0]) if len(vals) > 0 else ""
             else:
                 content = "<br>".join(map(str, vals[1:])) if len(vals) > 1 else ""
                 
-            html += f"<td style='border: 1px solid #555; height: 35px; vertical-align: top; padding: 2px; word-wrap: break-word;'>{content}</td>"
+            html += f"<td style='border: 1px solid #555; height: 35px; width: 50px; vertical-align: top; padding: 2px;'>{content}</td>"
         html += "</tr>"
     html += "</table>"
     st.markdown(html, unsafe_allow_html=True)
