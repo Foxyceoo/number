720801702fae5c7c2dfe5ee61ebdafeff5eca01d
Foxyceoo <vanhidol2k3@gmail.com>
2026-07-02 14:49:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -171,8 +171,8 @@ if uploaded_file:
 
             # Logic kẻ bảng
             is_new_line = (col_idx == 0)
-            is_beat_4 = ((col_idx + 1) % 8 == 0)
-            border_right = "0.5px solid #00008c" if (is_beat_4 or (col_idx + 1) == bits_per_page) else "1px solid #ff0000"
+            is_beat_4 = ((col_idx + 1) % 4 == 0)
+            border_right = "0.5px solid #00008c" if (is_beat_4 or (col_idx + 1) == bits_per_page) else "0px solid #ff0000"
             border_left = "0.5px solid #00008c" if is_new_line else "none"
 
             # --- BỔ SUNG ĐOẠN NÀY ĐỂ ĐỆM CHO ĐỦ NHỊP ---
